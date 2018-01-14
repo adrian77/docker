f7849ef3d59b873fc248ce6f5eab5e2217ccfb2d
git <adrian.szwej@gmail.com>
2018-01-14 01:42:41 +0100

    Finalizing the naming service

--- nameserver/to-add/entry.sh
+++ nameserver/to-add/entry.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+CFG_DIR=/config
+[ ! -z $1 ] && CFG_DIR=$1
+rm -rf /etc/bind
+mkdir /etc/bind
+cp -R $CFG_DIR/* /etc/bind
 chown -R root:bind /etc/bind
 /etc/init.d/rsyslog start
 /etc/init.d/bind9 start
-/bin/bash
+while [ true ]; do
+  sleep 100
+done
